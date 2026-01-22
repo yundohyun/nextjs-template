faea09de0389ccb4f1613b2086b6a74c4d051825
yundohyun <160732611+yundohyun@users.noreply.github.com>
2026-01-23 05:43:52 +0900

    chore: setup related `prettier`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierRecommended,
+  { rules: { "prettier/prettier": ["error", { endOfLine: "auto" }] } },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:

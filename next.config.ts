b8184fb9f7f801315c8a0466130fe8fd3c5255a8
yundohyun <160732611+yundohyun@users.noreply.github.com>
2026-02-09 20:02:25 +0900

    chore: add Ccoudflare pages setup and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+if (process.env.NODE_ENV == "development") await setupDevPlatform();
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
